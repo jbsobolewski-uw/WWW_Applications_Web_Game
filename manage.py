8cff9f3a470a77f2be8ff7715a47aeff835ebedd
js469693 <jb.sobolewsk@student.uw.edu.pl>
2026-05-19 00:29:48 +0200

    added .env file support for social media based authentication

--- manage.py
+++ manage.py
@@ -9,12 +9,15 @@ def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'web_game.settings')
     try:
         from django.core.management import execute_from_command_line
+        from dotenv import load_dotenv
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+
+    load_dotenv()
     execute_from_command_line(sys.argv)
 
 
