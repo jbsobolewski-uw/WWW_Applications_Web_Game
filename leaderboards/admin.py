c9db12e86a348f29f8720344522aa08eca3d2a46
js469693 <jb.sobolewsk@student.uw.edu.pl>
2026-05-19 21:03:33 +0200

    added leaderboards app to admin panel

--- leaderboards/admin.py
+++ leaderboards/admin.py
@@ -1,1 +1,9 @@
-# Register your models here.
+from django.contrib import admin
+from .models import GameRecord
+
+@admin.register(GameRecord)
+class GameRecordAdmin(admin.ModelAdmin):
+    # This instructs Django Admin on what columns to show
+    list_display = ('user', 'game_slug', 'difficulty', 'time_in_seconds', 'is_win', 'played_at')
+    list_filter = ('game_slug', 'difficulty', 'is_win')
+    search_fields = ('user__username', 'game_slug')
