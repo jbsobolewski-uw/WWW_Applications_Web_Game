c3b5257d020331a370290f508a43894ee5ed9e9c
js469693 <jb.sobolewsk@student.uw.edu.pl>
2026-05-31 17:10:17 +0200

    added favicons

--- web_game/urls.py
+++ web_game/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
-from web_game.settings import DEBUG
+from web_game.settings import DEBUG, STATIC_URL
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,6 +26,7 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('game/', include('game.urls')),
     path('leaderboards/', include('leaderboards.urls')),
+    path('favicon.ico', RedirectView.as_view(url=STATIC_URL + 'pages/favicon.ico')),
 ]
 
 if DEBUG:
